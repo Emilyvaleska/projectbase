6e1bb1a74a0381bd6b650eccc0613d276354e90b
emillyvaleska <emillyvaleska7@gmail.com>
2025-05-20 16:42:22 -0300

    variaveis, comentario, alert e console.log

---
+++ logicacomjs.js
@@ -0,0 +1,27 @@
+let bruna = 6.5;
+let carla = 8;
+let soma = bruna + carla 
+console.log("A soma é: " + soma)
+console.log("-------------------------")
+let pedro = 300;
+let joão = 900;
+let multiplicação = 300 - 900 
+let subtração = (pedro + joão)
+console.log("A sutrcação de Pedro e João é:"+ subtração+"R$")
+console.log("----------------------------")
+let arroz= 5;
+let macarrão= 3; 
+let Ovalorarmazenamento= arroz*macarrão
+console.log("A multiplicação é"+Ovalorarmazenamento+"R$")
+console.log("--------------------------------")
+let crechê = 30;
+let crianças = 20;
+let entregadepipocas=crechê/crianças
+console.log("A divisão é :"+entregadepipocas+"R$")
+console.log("--------------------------------")
+let val = 3000
+let meses = 12
+let totalanual= val/12*meses
+console.log("Val o seu "+décimo+"  é; "Parabéns!")
+
+
